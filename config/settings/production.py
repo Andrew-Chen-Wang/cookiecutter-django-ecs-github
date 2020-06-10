5a4670a2543735874d35bf947746ecb74900a491
Andrew-Chen-Wang <acwang02@gmail.com>
2020-06-10 13:47:35 -0400

    Test allowed hosts being all

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["asdfasq.de"])
+# ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["asdfasq.de"])
+ALLOWED_HOSTS = ["*"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
